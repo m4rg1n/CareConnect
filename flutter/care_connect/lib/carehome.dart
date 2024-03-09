9bec31eb67c7fe5088b2f7d97f25623f8315d794
Shiva-Surendran <107458185+Shiva-Surendran@users.noreply.github.com>
2024-03-09 22:56:59 +0530

    Update carehome.dart

--- flutter/care_connect/lib/carehome.dart
+++ flutter/care_connect/lib/carehome.dart
@@ -1,77 +1,87 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-  void main() {
-    runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Member Management App',
+      home: HomePage(),
+    );
   }
+}
 
-  class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        title: 'Member Management App',
-        home: HomePage(),
-      );
-    }
-  }
-
-  class HomePage extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Members'),
-          backgroundColor: Colors.green,
-        ),
-        body: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(20.0), 
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200], 
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Members'),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 3,
+                  backgroundColor: Colors.grey[200],
+                  shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10.0),
                   ),
-
-                  child: const Padding(
-                    padding: EdgeInsets.all(16.0), 
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(Icons.people, size: 48.0), 
-                        Text(
-                          'Beneficiary 1',
-                          style: TextStyle(fontSize: 18.0),
-                        ),
-                      ],
-                    ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.people, size: 48.0),
+                      Text(
+                        'Beneficiary 1',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 20.0),
-              
-                Row(
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle add member action
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 3,
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    IconButton(
-                      icon: const Icon(Icons.add_circle),
-                      iconSize: 48.0,
-                      onPressed: () {
-                      },
-                      color: Colors.black,
-                    ),
-                    const SizedBox(width: 10.0), 
-                    const Text(
+                    Icon(Icons.add_circle),
+                    SizedBox(width: 10.0),
+                    Text(
                       'Add Member',
                       style: TextStyle(fontSize: 18.0),
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-      );
-    }
+      ),
+    );
   }
+}
