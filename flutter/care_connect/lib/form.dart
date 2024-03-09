156faf5da308ca4b752c5ddabba6895857f6d791
Shiva-Surendran <107458185+Shiva-Surendran@users.noreply.github.com>
2024-03-09 19:45:10 +0530

    Create form for Caretaker

---
+++ flutter/care_connect/lib/form.dart
@@ -0,0 +1,120 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _submitform() {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(_formKey.currentContext!).showSnackBar(const SnackBar(content:Text("success")));
+
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: const Color.fromARGB(255, 248, 248, 248),
+        appBar: AppBar(
+          title: Text('Beneficiary Details'),
+          backgroundColor: Colors.green,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: InputDecoration(
+                        labelText: "Enter Name of beneficiary",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "enter";
+                      }
+                      return null;
+                    }),                SizedBox(
+                  height: 20,
+                ),
+                    TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: InputDecoration(
+                        labelText: "Enter Age of beneficiary",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    validator: (value) {
+                      if (value!.) {
+                        return "enter";
+                      }
+                      return null;
+                    }),
+                    
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(keyboardType:TextInputType.number,
+                    decoration: InputDecoration(
+                  labelText: "Enter Caretaker's Phone Number",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                ),   validator: (value) {
+                      if (value!.isEmpty) {
+                        return "enter something";
+                      }
+                       if (value.length!=10) {
+                        return "enter 10 DIGITS";
+                      }
+                      return null;
+                    }),  SizedBox(
+                  height: 20,
+                ),
+                    TextFormField(keyboardType:TextInputType.number,
+                    decoration: InputDecoration(
+                  labelText: "Enter Neighbour's Phone Number",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                ),   validator: (value) {
+                      if (value!.isEmpty) {
+                        return "enter something";
+                      }
+                       if (value.length!=10) {
+                        return "enter 10 DIGITS";
+                      }
+                      return null;
+                    }),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(height:50,width: double.infinity,child: ElevatedButton(onPressed: _submitform, child: Text("submit"))),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
