59b03cb144b2eac21ee2218810068b25b7301033
Shiva-Surendran <107458185+Shiva-Surendran@users.noreply.github.com>
2024-03-21 20:50:56 +0530

    Caretaker HomePage

---
+++ flutter/care_connect/lib/nav/home.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'form.dart'; 
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Members'),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                 
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 3,
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.people, size: 48.0),
+                      Text(
+                        'Beneficiary 1',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+ Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FormPage()),
+                  );                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 3,
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.add_circle),
+                    SizedBox(width: 10.0),
+                    Text(
+                      'Add Member',
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
